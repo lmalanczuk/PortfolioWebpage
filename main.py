b8b534206dc4144fd2915e2bb893dd512177f83d
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2024-01-08 17:19:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import streamlit as st
 import requests
-import streamlit_lottie as st_lottie
+from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="Moja strona", page_icon=":sunglasses:", layout="wide")
 
