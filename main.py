3519e623595b09b6f1d185145cd13ece9aefbc39
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2024-01-09 00:21:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,11 +42,11 @@ with st.container():
             Jestem studentem drugiego roku Informatyki na Uniwersytecie Ekonomicznym w Katowicach i jestem zafascynowany światem programowania 
             oraz tworzeniem gier i aplikacji. Moja pasja do programowania rozpoczęła się od Pythona, który jest moim głównym językiem programowania. 
             Aktywnie rozwijam się w programowaniu w Pythonie, zdobywając doświadczenie w różnych dziedzinach, takich jak web development, data science czy machine learning.
-            
+            \n
             Jednak nie ograniczam się tylko do Pythona. Chcę poszerzać swoje umiejętności, dlatego również rozwijam się w C# i UNITY.
             Tworzenie gier i aplikacji za pomocą tych technologii daje mi możliwość wykorzystania swojej kreatywności i umiejętności programistycznych. 
             Moje projekty obejmują zarówno rozrywkowe gry, które dostarczają radość i emocje, jak i praktyczne aplikacje, które mogą być przydatne w codziennym życiu.
-        
+            \n
         Zapraszam do zapoznania się z moimi projektami i do kontaktu, jeśli jesteś zainteresowany współpracą lub masz pytania
             """
         )
